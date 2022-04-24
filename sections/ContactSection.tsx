8de53ede6565c52d38322a560966b49f2a0c8ad1
helloworld <vital.pereg@mail.ru>
2022-04-24 15:54:26 +0300

    почти получилось сделать contacts

--- sections/ContactSection.tsx
+++ sections/ContactSection.tsx
@@ -1,13 +1,45 @@
 import { Formik, Field, Form } from "formik";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import * as Yup from "yup";
 
 const SignupSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
 });
 
+const MIN_TEXTAREA_HEIGHT = 58;
+
+const MessageComp = () => {
+  const textareaRef = useRef(null);
+  const [value, setValue] = useState("");
+  const onChange = (event: any) => setValue(event.target.value);
+
+  useLayoutEffect(() => {
+    // @ts-ignore
+    textareaRef.current.style.height = "inherit";
+    // @ts-ignore
+    textareaRef.current.style.height = `${Math.max(
+      // @ts-ignore
+      textareaRef.current.scrollHeight,
+      MIN_TEXTAREA_HEIGHT
+    )}px`;
+  }, [value]);
+
+  return (
+    <textarea
+      name="message"
+      id={"message"}
+      value={value}
+      onChange={onChange}
+      ref={textareaRef}
+      placeholder="Your Message"
+    ></textarea>
+  );
+};
+
 const ContactSection = () => {
   return (
     <section id="ContactUs" className="ContactSection">
+      <div className="triangle"></div>
       <div className="container">
         <h2>CONTACTS US</h2>
         <div className="rightSide">
@@ -17,7 +49,8 @@ const ContactSection = () => {
               name: "",
               email: "",
               phone: "",
-              // message: "",
+              message: "",
+              politic: false,
             }}
             validationSchema={SignupSchema}
             onSubmit={async (values: any) => {
@@ -41,7 +74,7 @@ const ContactSection = () => {
 
                 <Field id="phone" name="phone" placeholder="Your Phone" />
 
-                {/*<Field id="message" name="message" placeholder="Your Message" />*/}
+                <Field id="message" name="message" component={MessageComp} />
 
                 <div className="buttonWrap">
                   <button type="submit">Send</button>
